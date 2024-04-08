64b7c971ad500ad9f83ac8b49df8e0c8d581b437
Henry <henry.bogita@student.moringaschool.com>
2024-04-08 23:26:26 +0300

    Completed assignment

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,31 @@
-
 import requests
 import json
 
 class GetPrograms:
+    def __init__(self, url):
+        self.url = url
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        response = requests.get(self.url)
+        return response.content
 
-    response = requests.get(URL)
-    return response.content
+    def program_agencies(self):
+        programs_list = []
+        response_content = self.get_programs()
+        try:
+            programs = json.loads(response_content)
+            print(f"Type of programs: {type(programs)}") # Debugging line
+            print(f"Content of programs: {programs}") # Debugging line
+            for program in programs:
+                programs_list.append(program["agency"])
+        except json.JSONDecodeError:
+            print("Error: Response content is not valid JSON.")
+            print(response_content)
+        return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+if __name__ == "__main__":
+    url = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    programs = GetPrograms(url)
+    agencies = programs.program_agencies()
+    for agency in set(agencies):
+        print(agency)
